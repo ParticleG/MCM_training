c14d053a7a75cf0a4ecf7c4bd2e412677bde1125
Particle_G <1135989508@qq.com>
2021-01-03 23:01:39 +0800

    - Update question 3.

--- question_1.py
+++ question_1.py
@@ -6,15 +6,16 @@ from calculate_group_size import calculate_group_size
 def check_disease(_total_population, _infection_rate, _previous_group_size, _positive_group_cumulated,
                   _false_negative_rate, _false_positive_rate, _recessive_rate):  # b,a,beta,first
     _next_population, _negative_expect = calculate_group_size(_total_population, _infection_rate, 0, _previous_group_size)
+    _temp_false_negative_rate = (_next_population - _negative_expect) / _negative_expect * _false_negative_rate
     _positive_group_count = (
             Dec(_total_population) / Dec(_next_population) * ((Dec(1) - _negative_expect / _next_population) +  # Original positive groups
                                                               # False positives in negatives
-                                                              _false_positive_rate * (Dec(1) - _false_negative_rate) * (Dec(1) - _recessive_rate) +
+                                                              _false_positive_rate * (Dec(1) - _temp_false_negative_rate) * (Dec(1) - _recessive_rate) +
                                                               # Positive groups in second check
-                                                              (Dec(1) - _false_negative_rate) * (
-                                                                      (Dec(1) - _false_negative_rate) * _recessive_rate + _false_negative_rate))
+                                                              (Dec(1) - _temp_false_negative_rate) * (
+                                                                      (Dec(1) - _temp_false_negative_rate) * _recessive_rate + _temp_false_negative_rate))
     )
-    _next_infection_rate = Dec(_infection_rate) / (Dec(1) - (Dec(1) - _false_positive_rate - _false_negative_rate - _recessive_rate) * (
+    _next_infection_rate = Dec(_infection_rate) / (Dec(1) - (Dec(1) - _false_positive_rate - _temp_false_negative_rate - _recessive_rate) * (
             _negative_expect / Dec(_next_population)))
     _check_counts = (_total_population / _next_population * (Dec(1) + _negative_expect / _next_population)) * _positive_group_cumulated
     _positive_group_cumulated *= _positive_group_count
@@ -38,19 +39,24 @@ if __name__ == "__main__":
     next_population = total_population
     next_infection_rate = infection_rate
     positive_group_cumulated = Dec(1)
+    first_time = True
     while True:
-        is_last_check = False
+        last_time = False
         if next_population < 1:
-            is_last_check = True
+            last_time = True
         if next_population > 10000:
             limit_check_size = 10000
         else:
             limit_check_size = next_population
+        if first_time is True:
+            first_time = False
+        else:
+            recessive_rate = Dec(0)
         next_population, next_infection_rate, temp_check_counts, positive_group_count, positive_group_cumulated = check_disease(
             next_population, next_infection_rate, limit_check_size, positive_group_cumulated, false_negative_rate, false_positive_rate, recessive_rate
         )
         check_counts += temp_check_counts
         first_time = False
-        if is_last_check is True and next_population < 1:
+        if last_time is True and next_population < 1:
             print(f"check_counts:{int(check_counts) + 1}")
             break
--- question_3.py
+++ question_3.py
@@ -3,29 +3,28 @@ from decimal import Decimal as Dec
 from calculate_group_size import calculate_group_size
 
 
-def check_disease(_total_population, _infection_rate, _previous_group_size, _positive_group_cumulated,
+def check_disease(_total_population, _infection_rate, _previous_group_size,
                   _false_negative_rate, _false_positive_rate, _recessive_rate):  # b,a,beta,first
-    _next_population, _negative_expect = calculate_group_size(_total_population, _infection_rate, 0, _previous_group_size)
-    _positive_group_count = (
-            Dec(_total_population) / Dec(_next_population) * ((Dec(1) - _negative_expect / _next_population) +  # Original positive groups
-                                                              # False positives in negatives
-                                                              _false_positive_rate * (Dec(1) - _false_negative_rate) * (Dec(1) - _recessive_rate) +
-                                                              # Positive groups in second check
-                                                              (Dec(1) - _false_negative_rate) * (
-                                                                      (Dec(1) - _false_negative_rate) * _recessive_rate + _false_negative_rate))
-    )
-    _next_infection_rate = Dec(_infection_rate) / (Dec(1) - (Dec(1) - _false_positive_rate - _false_negative_rate - _recessive_rate) * (
-            _negative_expect / Dec(_next_population)))
-    _check_counts = (_total_population / _next_population * (Dec(1) + _negative_expect / _next_population)) * _positive_group_cumulated
-    _positive_group_cumulated *= _positive_group_count
+    _group_size, _negative_expect = calculate_group_size(_total_population, _infection_rate, 0, _previous_group_size)
+    _temp_false_negative_rate = (_group_size - _negative_expect) / _negative_expect * _false_negative_rate
+    _negative_population = _total_population * (_negative_expect / _group_size)
+    _negative_infection_rate = _temp_false_negative_rate + _recessive_rate
+
+    _true_negative_group_size, _true_negative_expect = calculate_group_size(_negative_population, _negative_infection_rate, 0, _negative_population)
+    _true_negative_population = _negative_population * (_true_negative_expect / _true_negative_group_size)
+    _false_negative_population = _negative_population * (Dec(1) - _true_negative_expect / _true_negative_group_size)
+
+    _positive_population = _total_population * (Dec(1) - _negative_expect / _group_size) + _false_negative_population
+
+    _positive_infection_rate = _positive_population
     print(f"_total_population={_total_population}, "
-          f"_next_population={_next_population}, "
+          f"_group_size={_group_size}, "
           f"_negative_expect={_negative_expect}, "
-          f"_positive_group_count={_positive_group_count}, "
-          f"_positive_group_cumulated={_positive_group_cumulated}, "
+          f"_negative_population={_negative_population}, "
+          f"_positive_population={_positive_population}, "
           f"_next_infection_rate={_next_infection_rate}, "
           f"_check_counts={_check_counts}, ")
-    return _next_population, _next_infection_rate, _check_counts, _positive_group_count, _positive_group_cumulated
+    return _group_size, _next_infection_rate, _check_counts, _positive_group_count
 
 
 if __name__ == "__main__":
@@ -37,7 +36,6 @@ if __name__ == "__main__":
     check_counts = Dec(0)
     next_population = total_population
     next_infection_rate = infection_rate
-    positive_group_cumulated = Dec(1)
     while True:
         is_last_check = False
         if next_population < 1:
@@ -46,8 +44,8 @@ if __name__ == "__main__":
             limit_check_size = 10000
         else:
             limit_check_size = next_population
-        next_population, next_infection_rate, temp_check_counts, positive_group_count, positive_group_cumulated = check_disease(
-            next_population, next_infection_rate, limit_check_size, positive_group_cumulated, false_negative_rate, false_positive_rate, recessive_rate
+        next_population, next_infection_rate, temp_check_counts, positive_group_count = check_disease(
+            next_population, next_infection_rate, limit_check_size, false_negative_rate, false_positive_rate, recessive_rate
         )
         check_counts += temp_check_counts
         first_time = False
